208e1d62309b84b34f7ae6457aaf123d49be3622
owen-eternal <poesylyrics@gmail.com>
2021-05-20 09:29:52 +0200

    modified set.up

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from typing import Text
 from setuptools import setup
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
+with open("README.md", "r") as f:
+    long_description = f.read()
 
 setup(
     name='Flask-bucket',
